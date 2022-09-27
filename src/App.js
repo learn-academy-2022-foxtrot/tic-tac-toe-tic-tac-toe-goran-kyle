1be8d46be65734bf0f98b6c604df215194b677bf
Goran SD <g.cvetic23@gmail.com>
2022-09-27 11:57:16 -0700

    added some more features to our app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ const App = () => {
   console.log(squares);
 
   const [xTurn, setXTurn] = useState(true);
-  // const [winner, setWinner] = useState([
-  //   ["0", "1", "2"],
-  //   ["3", "4", "5"],
-  //   ["6", "7", "8"],
-  //   ["0", "3", "6"],
-  //   ["1", "4", "7"],
-  //   ["2", "5", "8"],
-  //   ["0", "4", "8"],
-  //   ["2", "4", "6"],
-  // ]);
+
   const winnerSeq = [
     ["0", "1", "2"],
     ["3", "4", "5"],
